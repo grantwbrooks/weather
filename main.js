9fd6db2e068831ea88565a3d7e82649839527196
grantwbrooks <grantwbrooks@gmail.com>
2017-07-28 18:59:30 -0400

    updates

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         var cityentered = $("#city").val();
         var apikey = "ed0ea140436fc30efff8a1e5fa2e9b79"
         $.get("http://api.openweathermap.org/data/2.5/weather?q="+cityentered+"&units=imperial&appid="+apikey+"", function(res) {
+            console.log(res.name);
             var html_string = "";
             html_string += "<h1>"+res.name+"</h1>";
             html_string += "<div><p>Temperature: "+res.main.temp+"</p></div>";
